0654a87295f6003853607c49426adde6ac1bd1ee
Nabil Qamar <nqamar@about.com>
2017-06-07 13:12:44 -0400

    improvements

--- src/p540/Solution.java
+++ src/p540/Solution.java
@@ -6,11 +6,12 @@ public class Solution {
 
 	// public static int[] inputArray = { 3, 3, 7, 7, 10, 11, 11 };
 	// public static int[] inputArray = { 1, 1, 2, 3, 3, 4, 4, 8, 8, 3, 3, 7, 7, 11, 11 };
-	public static int[] inputArray = { 3, 3, 6, 6, 4, 4, 1, 1, 6, 6, 2, 2, 9, 1, 1, 7, 7, 5, 5, 8, 8 };
+//	public static int[] inputArray = { 3, 3, 6, 6, 4, 4, 1, 1, 6, 6, 2, 2, 9, 1, 1, 7, 7, 5, 5, 8, 8 };
 	// public static int[] inputArray = { 1, 1, 2, 2, 4, 4, 5, 5, 9 };
-	// public static int[] inputArray = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+	 public static int[] inputArray = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
 
 	public int candidate = -1;
+	public int[] newArray = null;
 
 	public static void main(String[] args) {
 		System.out.println(new Solution().singleNonDuplicate(inputArray));
@@ -20,7 +21,7 @@ public class Solution {
 
 		System.out.println("nums:" + Arrays.toString(nums));
 
-		int[] newArray = null;
+		newArray = null;
 
 		int size = nums.length;
 		int middle = nums[(size / 2)];
@@ -34,12 +35,12 @@ public class Solution {
 			if (!isEven) {// if leftover is odd then attach to left and discard
 				newArray = new int[size / 2];
 				System.arraycopy(nums, size / 2 + 1, newArray, 0, size / 2);
-				singleNonDuplicate(newArray);
 			} else {// if leftover is even then attach to left and keep it
 				newArray = new int[size / 2 + 1];
 				System.arraycopy(nums, 0, newArray, 0, size / 2 + 1);
-				singleNonDuplicate(newArray);
 			}
+			singleNonDuplicate(newArray);
+
 		}
 
 		// peek to the right
@@ -47,12 +48,11 @@ public class Solution {
 			if (!isEven) {// if leftover is odd then attach to right and discard
 				newArray = new int[size / 2];
 				System.arraycopy(nums, 0, newArray, 0, size / 2);
-				singleNonDuplicate(newArray);
 			} else {// if leftover is even then attach to right and keep it
 				newArray = new int[size / 2 + 1];
 				System.arraycopy(nums, size / 2, newArray, 0, size / 2 + 1);
-				singleNonDuplicate(newArray);
 			}
+			singleNonDuplicate(newArray);
 		}
 
 		else candidate = middle;
